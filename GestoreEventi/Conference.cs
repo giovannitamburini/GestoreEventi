4b7a63b9749c896a0951e278821a28834e8f5f2e
giovannitamburini <giovatambu@gmail.com>
2023-09-18 18:30:27 +0200

    created Conference class & added its constructor and methods

---
+++ GestoreEventi/Conference.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Conference : Event
+    {
+        public string Speaker { get; set; }
+        public double Price { get; set; }
+
+        public Conference(string title, DateTime date, int maxCapacity, string speaker, double price) : base(title, date, maxCapacity)
+        {
+            this.Speaker = speaker;
+            this.Price = price;
+        }
+
+        public string DateFormatter(DateTime date)
+        {
+            return date.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+
+        public string PriceFormatter(double price)
+        {
+            return price.ToString("0.00");
+        }
+    }
+}
