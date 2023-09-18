200201dfb38a564d8885b3d9f84dd6751fcc22e4
giovannitamburini <giovatambu@gmail.com>
2023-09-18 15:05:45 +0200

    added method to add an event to the event list

--- GestoreEventi/EventsProgram.cs
+++ GestoreEventi/EventsProgram.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +12,19 @@ namespace GestoreEventi
         string Title { get; set; }
         List<Event> Events { get; set; }
 
+        // COSTRUTTORE
         public EventsProgram(string programTitle)
         {
             this.Title = programTitle;
             this.Events = new List<Event>(); 
+        }
+
+        // METODI
+
+        // metodo che aggiunge alla lista del programma eventi un Evento, passato come parametro al metodo
+        public void AddEventToProgramList(Event ev)
+        {
+            Events.Add(ev);
         }
     }
 }
